3e82e267bd2452223e6413af9fb0dfcd78aa2b52
Brooks Travis <brooks.travis@gmail.com>
2015-12-13 15:05:47 -0600

    Adding `from __future__ import unicode_literals` to all modules.

--- src/django_fields/__init__.py
+++ src/django_fields/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from pkg_resources import get_distribution, DistributionNotFound
 import os.path
 
--- src/django_fields/fields.py
+++ src/django_fields/fields.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import binascii
 import datetime
 import string
